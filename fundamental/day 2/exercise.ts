cf94fec1369ca1a605c0ed0bb08ac89c2a37592f
jordansmrd <jordansumardi@hotmail.com>
2024-10-30 22:14:11 +0700

    feat: add exercise day2

---
+++ fundamental/day 2/exercise.ts
@@ -0,0 +1,52 @@
+/** @format */
+
+// Write a code to check whether the number is odd or even
+// Example: 25 → odd number, 2 → even number
+let number: number = 25;
+console.log(number % 2 == 0 ? "even" : "odd");
+
+// Write a code to check whether the number is prime number or not
+// Example: 7 → 7 is a prime number
+// Example: 6 → 6 is not a prime number
+number = 3;
+let isPrime = number > 1 ? true : false;
+for (let i = 2; i < number; i++) {
+  if (number % i == 0) {
+    isPrime = false;
+  }
+}
+console.log(isPrime);
+
+// Write a code to find the sum of the numbers 1 to N
+// Example: 5 → 1 + 2 + 3 + 4 + 5 = 15
+// Example: 3 → 1 + 2 + 3 = 6
+let result: number = 0;
+number = 5;
+for (let i = 1; i <= number; i++) {
+  result += i;
+}
+console.log(result);
+
+// Write a code to find factorial of a number
+// Example: 4! → 4 x 3 x 2 x 1 = 24
+// Example: 6! → 6 x 5 x 4 x 3 x 2 x 1 = 720
+result = 1;
+number = 4;
+for (let i = 1; i <= number; i++) {
+  result *= i;
+}
+console.log(result);
+
+// Write a code to print the first N fibonacci numbers
+// Example: 15 → 610
+// 1,1,2,3,5,8,13, 21,
+number = 15;
+let temp1 = 0,
+  temp2 = 1;
+for (let i = 1; i < number; ++i) {
+  result = temp1 + temp2;
+  temp1 = temp2;
+  temp2 = result;
+}
+
+console.log(result);
